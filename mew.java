66871735ce9ec9b8ba32b4140cb1185056a3d25d
angel grueso <149785576+ingelkawkaw@users.noreply.github.com>
2025-01-28 19:21:41 +0800

    Create mew.java

---
+++ mew.java
@@ -0,0 +1,26 @@
+
+public class Exer1_grueso {
+       public static void main (String[] args){
+	  System.out.println ("   .         *     . ");
+	  System.out.println ("          .  *   .   ");
+	  System.out.println (" .          ° °      ");
+	  System.out.println ("   .       * ° *.     . ");
+	  System.out.println ("  .       ° * * °  . ");
+	  System.out.println ("         * ° * ° *      ");
+	  System.out.println (" .      ° * ° ° * °   .    ");
+	  System.out.println ("  .      *° * * °*  .   ");
+	  System.out.println ("         * ° * ° *     ");
+	  System.out.println (" .      ° * ° ° * °    .  ");
+	  System.out.println (".     * ° * ° ° * ° *  .   ");
+	  System.out.println ("   .   ° * ° * ° * °     .  ");
+	  System.out.println ("  .    * * ° * ° * * .   .  ");
+	  System.out.println (" .    * ° * ° ° * ° *.   .    ");
+	  System.out.println ("   .° * ° * ° ° * ° * °.  .");
+	  System.out.println (" .* ° * ° * ° ° * ° * ° *   .  ");
+	  System.out.println (".** ° * ° * ° ° * ° * ° ** .  ");
+	  System.out.println ("  .  . oo  IIIII  oo       .     ");
+	  System.out.println (".     ■■■■ IIIII ■■■■        .  ");
+	  System.out.println ("   .  ■■■■ IIIII ■■■■    .     ");
+	  }
+}
+
